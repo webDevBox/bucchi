ffcb8825a5a6a9b5e2acbe3d7ff1f5cdf27f0d56
khubaib-dev <137499579+khubaib-dev@users.noreply.github.com>
2023-08-25 08:19:25 +0500

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                             aria-expanded="false">
 
                             <span class="avatar"><img class="round"
-                                    src="{{ asset('app-assets/images/portrait/small/avatar-s-.jpg')}}" alt="avatar"
+                                    src="{{ asset('app-assets/images/portrait/small/avatar-s-11.jpg')}}" alt="avatar"
                                     height="40" width="40"><span class="avatar-status-online"></span></span>
                             <div class="user-nav d-sm-flex d-none"><span class="user-name font-weight-bolder ml-1">{{ auth()->user()->name }}</span><span class="user-status">Admin</span></div>
                         </a>
